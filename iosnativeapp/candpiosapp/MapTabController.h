5158b79eed5f7df5b687be61fee5a150401908ea
David Mojdehi <dmojdehi@mac.com>
2011-12-31 18:35:42 +0000

    - added basic tabs (map, list, my c&p) - removed iPad storyboard (re-use iPhone storyboard for now)
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@44 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

---
+++ iosnativeapp/candpiosapp/MapTabController.h
@@ -0,0 +1,13 @@
+//
+//  MapTabController.h
+//  candpiosapp
+//
+//  Created by David Mojdehi on 12/30/11.
+//  Copyright (c) 2011 Coffee and Power Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapTabController : UIViewController
+
+@end
