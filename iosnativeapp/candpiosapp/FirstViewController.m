89fd776e9427958ef27cb6db46e849b64c3d8fa0
David Mojdehi <dmojdehi@mac.com>
2011-12-31 01:34:45 +0000

    - changed copyright to Coffee and Power Inc (and made it the default for new files)
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@43 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

--- iosnativeapp/candpiosapp/FirstViewController.m
+++ iosnativeapp/candpiosapp/FirstViewController.m
@@ -3,7 +3,7 @@
 //  candpiosapp
 //
 //  Created by David Mojdehi on 12/30/11.
-//  Copyright (c) 2011 Mindful Bear Apps. All rights reserved.
+//  Copyright (c) 2011 Coffee and Power Inc. All rights reserved.
 //
 
 #import "FirstViewController.h"
