2c84a36a817d8f9c5e91e04b049c893e2a23811f
David Mojdehi <dmojdehi@mac.com>
2012-01-02 22:06:56 +0000

    - allow rotation (of all views)
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@58 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

---
+++ iosnativeapp/candpiosapp/Views/EmptyController.h
@@ -0,0 +1,13 @@
+//
+//  EmptyController.h
+//  candpiosapp
+//
+//  Created by David Mojdehi on 1/2/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmptyController : UIViewController
+
+@end
