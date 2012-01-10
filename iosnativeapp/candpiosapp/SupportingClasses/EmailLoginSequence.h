69677800c1cab63b198c5e55819c6067a9f7bef8
David Mojdehi <dmojdehi@mac.com>
2012-01-10 19:06:00 +0000

    - email login: added (empty) support classes
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@71 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

---
+++ iosnativeapp/candpiosapp/SupportingClasses/EmailLoginSequence.h
@@ -0,0 +1,13 @@
+//
+//  EmailLoginSequence.h
+//  candpiosapp
+//
+//  Created by David Mojdehi on 1/10/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EmailLoginSequence : NSObject
+
+@end
---
+++ iosnativeapp/candpiosapp/SupportingClasses/EmailLoginSequence.m
@@ -0,0 +1,13 @@
+//
+//  EmailLoginSequence.m
+//  candpiosapp
+//
+//  Created by David Mojdehi on 1/10/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#import "EmailLoginSequence.h"
+
+@implementation EmailLoginSequence
+
+@end
