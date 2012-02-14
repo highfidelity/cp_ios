32ddd142cf75e0ba42618b8d53edb12521066e1b
Alexi Kostibas <alexi>
2012-02-14 19:12:42 +0000

    FaceToFace: adding stub classes and updates to the storyboard
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@163 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

---
+++ iosnativeapp/candpiosapp/FaceToFace/FaceToFaceInviteController.h
@@ -0,0 +1,13 @@
+//
+//  FaceToFaceInviteController.h
+//  candpiosapp
+//
+//  Created by Alexi (Love Machine) on 2012/02/14.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FaceToFaceInviteController : UIViewController
+
+@end
