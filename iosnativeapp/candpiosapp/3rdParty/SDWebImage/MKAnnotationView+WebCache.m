dbf650bc60c1c60d1d6c3331d7969d8850fa576c
Emmanuel Crouvisier <emcro@emcro.com>
2012-02-28 00:49:45 +0000

    Half-sized checkout pin without dwarved callouts
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@241 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

--- iosnativeapp/candpiosapp/3rdParty/SDWebImage/MKAnnotationView+WebCache.m
+++ iosnativeapp/candpiosapp/3rdParty/SDWebImage/MKAnnotationView+WebCache.m
@@ -13,28 +13,36 @@
 
 @implementation MKAnnotationView(WebCache)
 
-- (void)setNumberedPin:(NSInteger)number hasCheckins:(BOOL)checkins {
-    UILabel *numberLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 15, 93, 20)];
+- (void)setNumberedPin:(NSInteger)number hasCheckins:(BOOL)checkins {    
+    CGFloat fontSize = 20;
+    NSString *imageName;
+
+    UILabel *numberLabel = [[UILabel alloc] init];
+
+    // If no one is currently checked in, use smaller image + font size
+    if (checkins) {
+        imageName = @"pin-checkedin";
+        numberLabel.frame = CGRectMake(0, 15, 93, 20);
+    }
+    else {
+//        self.alpha = 0.4;        
+        numberLabel.frame = CGRectMake(0, 9, 54, 12);
+        imageName = @"pin-checkedout";
+        fontSize = 12;
+    }
+    
+    [self setImage:[UIImage imageNamed:imageName]];
+    
+    // Add number label
     
     numberLabel.backgroundColor = [UIColor clearColor];
     numberLabel.opaque = NO;
     numberLabel.textColor = [UIColor whiteColor];
-    numberLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:20.0];
+    numberLabel.font = [UIFont fontWithName:@"HelveticaNeue-Bold" size:fontSize];
     numberLabel.textAlignment = UITextAlignmentCenter;
     
     numberLabel.text = [NSString stringWithFormat:@"%d", number];
     [self addSubview:numberLabel];
-    
-    // If no one is currently checked in, set alpha to 50% and decrease size of pin
-    if (checkins) {
-        [self setImage:[UIImage imageNamed:@"pin-checkedin"]];
-    }
-    else {
-//        self.alpha = 0.4;        
-        [self setImage:[UIImage imageNamed:@"pin-checkedout"]];
-
-        self.transform = CGAffineTransformScale(self.transform, 0.6, 0.6);
-    }    
 }
 
 - (void)setImage:(UIImage *)newImage fancy:(BOOL)fancyImage {
