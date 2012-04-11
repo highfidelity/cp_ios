a1d844b4955f04b8acd6cb5a21ba0ef0b28aaaaf
Stephen Birarda <commit@birarda.com>
2012-04-11 18:32:30 +0000

    todo in mapdataset
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@548 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

--- iosnativeapp/candpiosapp/SupportingClasses/MapDataSet.h
+++ iosnativeapp/candpiosapp/SupportingClasses/MapDataSet.h
@@ -11,6 +11,12 @@
 #import <MapKit/MKGeometry.h>
 
 @interface MapDataSet : NSObject
+
+// TODO: Although it might not matter because I don't think the map comes back to grab it
+// (it simply alloc-inits a completely new dataset)
+// this, like activeUsers, should really not be mutable so that nobody grabs it and
+// changes the data from underneath the map 
+
 @property (nonatomic, readonly, strong) NSMutableArray *annotations;
 @property (nonatomic, strong) NSDate *dateLoaded;
 @property (nonatomic, assign) MKMapRect regionCovered;
