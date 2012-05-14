84ed784af6b3110802da8723ed34284dfb54686d
andyast <andyast@gmail.com>
2012-05-14 16:16:52 +0000

    #17339 - Hide the Wall, move the notifications - Putting Auto Check In options in seperate view.
    git-svn-id: http://svn.worklist.net/svn/repos/candpmobi@744 6f5965ea-d3b0-40a2-9f37-23c37bdfaddc

--- iosnativeapp/candpiosapp/Settings/ProfileNotificationsViewController.h
+++ iosnativeapp/candpiosapp/Settings/ProfileNotificationsViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ProfileNotificationsViewController : UITableViewController
+@interface ProfileNotificationsViewController : UIViewController
 
 @end
--- iosnativeapp/candpiosapp/Settings/ProfileNotificationsViewController.m
+++ iosnativeapp/candpiosapp/Settings/ProfileNotificationsViewController.m
@@ -17,6 +17,7 @@
 #define kInCityText @"in city"
 
 @interface ProfileNotificationsViewController () <UIActionSheetDelegate>
+
 @property (weak, nonatomic) IBOutlet UIButton *venueButton;
 @property (weak, nonatomic) IBOutlet UISwitch *checkedInOnlySwitch;
 @property (weak, nonatomic) IBOutlet UISwitch *quietTimeSwitch;
@@ -25,7 +26,6 @@
 @property (weak, nonatomic) IBOutlet UIButton *quietToButton;
 @property (weak, nonatomic) IBOutlet UISwitch *contactsOnlyChatSwitch;
 @property (weak, nonatomic) IBOutlet UILabel *chatNotificationLabel;
-@property (weak, nonatomic) IBOutlet UISwitch *globalCheckinSwitch;
 @property (nonatomic, strong) NSMutableArray *placesArray;
 @property (nonatomic, strong) CLLocationManager *locationManager;
 @property (weak, nonatomic) IBOutlet UIView *headerView;
@@ -37,7 +37,6 @@
 - (IBAction)quietToClicked:(UIButton *)sender;
 - (IBAction)quietTimeValueChanged:(UISwitch *)sender;
 - (IBAction)anyoneChatSwitchChanged:(id)sender;
-- (IBAction)globalCheckinChanged:(id)sender;
 
 
 @property(strong) NSDate *quietTimeFromDate;
@@ -57,7 +56,6 @@
 @synthesize chatNotificationLabel = _chatNotificationLabel;
 @synthesize quietTimeFromDate = _quietTimeFromDate;
 @synthesize quietTimeToDate = _quietTimeToDate;
-@synthesize globalCheckinSwitch = _globalCheckinSwitch;
 @synthesize placesArray = _placesArray;
 @synthesize locationManager = _locationManager;
 @synthesize headerView = _headerView;
@@ -79,14 +77,6 @@
 	// Do any additional setup after loading the view.
     [self venueButton].titleEdgeInsets = UIEdgeInsetsMake(0, 10, 0, 10);
     
-    // If automaticCheckins is disabled, hide the table view unless changed
-    BOOL automaticCheckins = [DEFAULTS(object, kAutomaticCheckins) boolValue];
-    
-    self.globalCheckinSwitch.on = automaticCheckins;
-    
-    if (automaticCheckins) {    
-        [self setupPlacesArray];
-    }
 }
 
 -(void)viewWillAppear:(BOOL)animated
@@ -107,7 +97,6 @@
     [self setQuietTimeFromDate:nil];
     [self setQuietTimeToDate:nil];
     [self setChatNotificationLabel:nil];
-    [self setGlobalCheckinSwitch:nil];
     [self setHeaderView:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
@@ -287,9 +276,6 @@
                                                self.anyoneChatView.frame.size.width,
                                                self.anyoneChatView.frame.size.height);
         
-        self.headerView.frame = CGRectMake(0,0, 
-                                            self.headerView.frame.size.width,
-                                            quietTime ? 330 : 290);
     }];
     
     
@@ -304,111 +290,6 @@
     NSString *dateString = [timeFormatter stringFromDate: timeValue];
     
     return dateString;
-}
-
-- (void)setupPlacesArray {
-    if (!self.placesArray) {
-        self.placesArray = [[NSMutableArray alloc] init];
-    }
-    
-    NSArray *pastVenues = DEFAULTS(object, kUDPastVenues);
-    
-    for (NSData *encodedObject in pastVenues) {
-        CPVenue *venue = (CPVenue *)[NSKeyedUnarchiver unarchiveObjectWithData:encodedObject];
-        
-        if (venue && venue.name) {
-            //            NSLog(@"venue found: %@", venue.name);
-            [self.placesArray addObject:venue];
-        }
-    }
-    
-    NSArray *sortedArray;
-    
-    sortedArray = [self.placesArray sortedArrayUsingComparator:^(id a, id b) {
-        NSString *first = [(CPVenue *)a name];
-        NSString *second = [(CPVenue *)b name];
-        return [first compare:second];
-    }];
-    
-    self.placesArray = [sortedArray mutableCopy];
-}
-
-- (IBAction)globalCheckinChanged:(UISwitch *)sender {
-    // Store the choice in NSUserDefaults
-    SET_DEFAULTS(Object, kAutomaticCheckins, [NSNumber numberWithBool:sender.on]);
-    
-    if (!sender.on) {
-        // Disable auto checkins
-        
-        for (CPVenue *venue in self.placesArray) {
-            [CPAppDelegate stopMonitoringVenue:venue];
-        }
-        
-        [self.placesArray removeAllObjects];
-        
-        // Clear out all currently monitored regions in order to stop using geofencing altogether
-        for (CLRegion *reg in [[CPAppDelegate locationManager] monitoredRegions]) {
-            [[CPAppDelegate locationManager] stopMonitoringForRegion:reg];
-        }
-        
-        [FlurryAnalytics logEvent:@"automaticCheckinsDisabled"];
-    }
-    else {
-        [self setupPlacesArray];
-        
-        // Iterate over all past venues to start monitoring those with autoCheckin enabled        
-        for (CPVenue *venue in self.placesArray) {
-            NSLog(@"auto: %i, venue: %@", venue.autoCheckin, venue.name);
-            if (venue.autoCheckin) {
-                [CPAppDelegate startMonitoringVenue:venue];
-            }
-        }
-        
-        [FlurryAnalytics logEvent:@"automaticCheckinsEnabled"];
-    }
-    
-    [self.tableView reloadData];
-}
-
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    if (self.placesArray.count > 0) {
-        return 1;
-    }
-    else {
-        return 0;
-    }
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    if (self.placesArray.count > 0) {
-        return self.placesArray.count;
-    }
-    else {
-        return 0;
-    }
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *CellIdentifier = @"AutoCheckinCell";
-    AutoCheckinCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    
-    CPVenue *venue = [self.placesArray objectAtIndex:indexPath.row];
-    
-    if (venue) {
-        cell.venueName.text = venue.name;
-        cell.venueAddress.text = venue.address;
-        cell.venue = venue;
-        
-        cell.venueSwitch.on = venue.autoCheckin;
-    }
-    
-    return cell;
 }
 
 @end
