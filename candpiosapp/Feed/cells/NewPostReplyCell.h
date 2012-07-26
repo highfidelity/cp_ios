15daff4d9c05c67b625603f4ad4aaf18c99732e9
Stephen Birarda <commit@birarda.com>
2012-07-26 13:36:11 -0700

    added new cell types for replies, ugly button to allow commenting on existing post

---
+++ candpiosapp/Feed/cells/NewPostReplyCell.h
@@ -0,0 +1,13 @@
+//
+//  NewPostReplyCell.h
+//  candpiosapp
+//
+//  Created by Stephen Birarda on 7/25/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#import "NewPostCell.h"
+
+@interface NewPostReplyCell : NewPostCell
+
+@end
---
+++ candpiosapp/Feed/cells/PostReplyCell.h
@@ -0,0 +1,13 @@
+//
+//  PostReplyCell.h
+//  candpiosapp
+//
+//  Created by Stephen Birarda on 7/25/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#import "PostUpdateCell.h"
+
+@interface PostReplyCell : PostUpdateCell
+
+@end
