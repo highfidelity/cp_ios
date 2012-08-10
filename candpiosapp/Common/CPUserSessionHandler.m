9e71170e14dd1c7f5a844fed89f12e1ac9706b4e
Stephen Birarda <commit@birarda.com>
2012-08-10 13:00:53 -0600

    some comments in CPUserSessionHandler

--- candpiosapp/Common/CPUserSessionHandler.m
+++ candpiosapp/Common/CPUserSessionHandler.m
@@ -33,9 +33,13 @@ static CPUserSessionHandler *sharedHandler;
     
     while ([CLLocationManager locationServicesEnabled] && [CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined) {
         // looping until we have a location service acceptance / denial
+        // if the user hasn't given a location service status (accept or deny) to our app
+        // the alert will show up before the signup modal drops and must be interacted with before it does
     }
     
     // we have a current user so we are good to ask for push and sync the user
+    
+    // doesn't matter if this is called after UA has already taken off, the AppDelegate won't do it twice
     [CPAppDelegate setupUrbanAirship];
     [CPAppDelegate pushAliasUpdate];
     
