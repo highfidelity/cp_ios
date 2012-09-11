cad88ba9900d7f2fccc9e542495bedbd427c87e4
Stephen Birarda <commit@birarda.com>
2012-09-11 14:13:25 -0400

    fix a crash from doing UI changes on a background thread

--- candpiosapp/Common/CPSwipeableTableViewCell/CPUserActionCell.m
+++ candpiosapp/Common/CPSwipeableTableViewCell/CPUserActionCell.m
@@ -197,9 +197,6 @@
     }
 }
 
-- (void) highlightInBackground {
-    [self performSelectorInBackground:@selector(highlight) withObject:self];
-}
 - (void) highlight {
     // mimic row selection - highlight and push the child view
     [UIView animateWithDuration:HIGHLIGHT_DURATION animations:^{
@@ -219,7 +216,7 @@
 
 -(void)touchesBegan:(NSSet*)touches withEvent:(UIEvent*)evt {
     if (!self.areActionButtonsVisible) {
-        [self highlightInBackground];
+        [self highlight];
     }
 }
 
@@ -229,7 +226,7 @@
     // handle taps
     if (!self.areActionButtonsVisible) {
         // mimic row selection - highlight and push the child view
-        [self highlightInBackground];
+        [self highlight];
         if (recognizer.state == UIGestureRecognizerStateEnded) {
             // return immediately.. next view must be initialized on main thread
             [self performSelectorOnMainThread:@selector(handleTap) withObject:nil waitUntilDone:NO];
