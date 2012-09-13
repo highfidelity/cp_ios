251464bb11478e724a441e2c74ebf234716ca9c4
Kevin Utecht <kevin@kutecht.com>
2012-09-13 10:01:27 -0500

    Added venue name to warning message.

--- candpiosapp/Feed/FeedViewController.m
+++ candpiosapp/Feed/FeedViewController.m
@@ -594,7 +594,7 @@ typedef enum {
                                         offsetFromUtc:[sectionVenueFeed.venue.utc intValue]];
             }
             else {
-                NSLog(@"Warning: venue missing a utc (offset from UTC)");
+                NSLog(@"Warning: venue %@ is missing a utc (offset from UTC)", sectionVenueFeed.venue.name);
                 // defaulting to the date of the post, the time displayed will be off by the offset of the venue!
                 firstPostDate = [[sectionVenueFeed.posts objectAtIndex:0] date];
             }
