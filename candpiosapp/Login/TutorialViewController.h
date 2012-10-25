59e069eae8830949e7bde1fa22b315c4c2c622eb
Tomáš Horáček <tomas.horacek@gmail.com>
2012-10-25 04:20:29 +0200

    add tutorial to settings

---
+++ candpiosapp/Login/TutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  TutorialViewController.h
+//  candpiosapp
+//
+//  Created by Tomáš Horáček on 10/23/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+@interface TutorialViewController : UIViewController <UIScrollViewDelegate>
+
+@property (nonatomic, assign) BOOL isShownFromLeft;
+
+@end
