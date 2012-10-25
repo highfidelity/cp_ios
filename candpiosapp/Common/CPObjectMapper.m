905ed34b5b6ebf5e31b966f39b1c3d32f26232dc
Stephen Birarda <commit@birarda.com>
2012-10-24 20:17:55 -0400

    update code in CPObjectMapper for RestKit 0.20, fix prefix file for AFNetworking as pod

--- candpiosapp/Common/CPObjectMapper.m
+++ candpiosapp/Common/CPObjectMapper.m
@@ -14,18 +14,15 @@
 + (void)initialize
 {
     // make sure that RestKit is ready to go
-    [RKObjectManager objectManagerWithBaseURLString:kCandPWebServiceUrl];
+    [RKObjectManager managerWithBaseURL:[NSURL URLWithString:kCandPWebServiceUrl]];
     
     // show the network activity spinner during requests
-    [RKObjectManager sharedManager].client.requestQueue.showsNetworkActivityIndicatorWhenBusy = YES;
+    [AFNetworkActivityIndicatorManager sharedManager].enabled = YES;
 }
 
 + (void)setupAllRKObjectMappings
 {
-    RKObjectMappingProvider *sharedMapper = [RKObjectManager sharedManager].mappingProvider;
     
-    [sharedMapper setMapping:[self venueRKObjectMapping] forKeyPath:@"venues"];
-    [sharedMapper setMapping:[self userObjectMapping] forKeyPath:@"users"];
 }
 
 + (RKObjectMapping *)venueRKObjectMapping
@@ -35,21 +32,22 @@
     if (!_venueRKObjectMapping) {
         _venueRKObjectMapping = [RKObjectMapping mappingForClass:[CPVenue class]];
         
-        [_venueRKObjectMapping mapKeyPath:@"id" toAttribute:@"venueID"];
-        [_venueRKObjectMapping mapAttributes:@"name", @"address", nil];
-        [_venueRKObjectMapping mapKeyPath:@"foursquare_id" toAttribute:@"foursquareID"];
-        [_venueRKObjectMapping mapKeyPath:@"photo_url" toAttribute:@"photoURL"];
-        
-        [_venueRKObjectMapping mapKeyPath:@"checked_in_now" toAttribute:@"checkinCount"];
-        
         // the API usually allows for an interval to be passed when asking for venue info
         // as of right now the mobile app never passes this and it's always 7 days
         
         // if we decide that in some places we want a different interval we should switch
         // the venue property to also be intervalCheckinCount, and maintain another
         // parameter so that for a given venue we know which interval it is for
-        [_venueRKObjectMapping mapKeyPath:@"checkins_for_interval" toAttribute:@"weeklyCheckinCount"];
         
+        [_venueRKObjectMapping addAttributeMappingsFromDictionary:@{
+            @"id" : @"venueID",
+            @"name": @"name",
+            @"address": @"address",
+            @"foursquare_id": @"foursquareID",
+            @"photo_url": @"photoURL",
+            @"checked_in_now": @"checkedInNow",
+            @"checkins_for_interval": @"weeklyCheckinCount"
+         }];
     }
     
     return _venueRKObjectMapping;
@@ -62,13 +60,14 @@
     if (!_userObjectMapping) {
         _userObjectMapping = [RKObjectMapping mappingForClass:[CPUser class]];
         
-        [_userObjectMapping mapKeyPath:@"id" toAttribute:@"userID"];
-        [_userObjectMapping mapAttributes:@"nickname",
-                                          @"major_job_category",
-                                          @"minor_job_category",
-                                          nil];
-        [_userObjectMapping mapKeyPath:@"filename" toAttribute:@"photoURLString"];
-        [_userObjectMapping mapKeyPath:@"is_contact" toAttribute:@"isContact"];
+        [_userObjectMapping addAttributeMappingsFromDictionary:@{
+            @"id" : @"userID",
+            @"nickname": @"nickname",
+            @"photo_url" : @"photoURL",
+            @"major_job_category": @"majorJobCategory",
+            @"minor_job_category": @"minorJobCategory",
+            @"is_contact" : @"isContact"
+         }];
     }
     
     return _userObjectMapping;
