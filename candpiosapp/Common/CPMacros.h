59a4238ed2f453874b2a95bcda9fd8f2fd756f14
Stephen Birarda <commit@birarda.com>
2012-11-13 18:20:31 -0500

    conditional text offset, macro file for common macros

---
+++ candpiosapp/Common/CPMacros.h
@@ -0,0 +1,21 @@
+//
+//  CPMacros.h
+//  candpiosapp
+//
+//  Created by Stephen Birarda on 11/13/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+#ifndef candpiosapp_CPMacro_h
+#define candpiosapp_CPMacro_h
+
+// redefine NSLog so it throws to TF and prints it all pretty
+#define NSLog(__FORMAT__, ...) TFLog((@"%s [Line %d] " __FORMAT__), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+
+// define a way to quickly grab the app delegate
+#define CPAppDelegate (AppDelegate *)[UIApplication sharedApplication].delegate
+
+// quick way to check iOS version
+#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] floatValue] >= v)
+
+#endif
