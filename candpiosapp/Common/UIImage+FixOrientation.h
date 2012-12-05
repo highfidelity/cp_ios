c6b73458eff24378da914239c3b61d3d4fb56da0
Stephen Birarda <commit@birarda.com>
2012-12-04 19:05:05 -0500

    use new category to fix orientation of profile picture

---
+++ candpiosapp/Common/UIImage+FixOrientation.h
@@ -0,0 +1,17 @@
+//
+//  UIImage+FixOrientation.h
+//  candpiosapp
+//
+//  Created by Stephen Birarda on 12/4/12.
+//  Copyright (c) 2012 Coffee and Power Inc. All rights reserved.
+//
+
+// stolen from https://gist.github.com/1064652
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (FixOrientation)
+
+- (UIImage *)fixOrientation;
+
+@end
